6ab4fc0657d25118406e1590eb680ebd9d5f114f
Pendelluft <c.marie.s@hotmail.com>
2017-02-14 08:59:52 -0500

    Added Course class and made changes to Student and Instructor.

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,25 @@
 /**
  * Created by ethan on 2017-02-07.
  */
+
+import java.util.ArrayList;
+
 public class Student extends Person {
     String major;
+    boolean fulltime;
+    ArrayList<Course> coursList;
 
-    public Student(String name, String emailAddress, String major) {
+    public Student(String name, String emailAddress, String major, boolean hasNoLife) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime = hasNoLife;
     }
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c){
+        coursList.add(c);
     }
 }
